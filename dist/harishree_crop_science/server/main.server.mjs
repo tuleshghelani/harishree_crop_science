f37f97ecd593fcd32016d766ad9425fe6753f12c
tuleshghelani <ghelanitulesh@gmail.com>
2026-02-26 11:36:00 +0530

    fix: update .gitignore to correctly ignore the /dist directory

---
+++ dist/harishree_crop_science/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2HYK75R6.mjs";import"./chunk-MNTE7ZU2.mjs";import"./chunk-XKLREJ5M.mjs";import"./chunk-KZTHYQW5.mjs";import"./chunk-3USVL2KH.mjs";import"./chunk-FWUDPXWX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
